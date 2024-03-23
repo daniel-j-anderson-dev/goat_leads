a2404e91a628d64a433002123ea247999053633b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-23 11:31:09 -0400

    Added axum and sea-orm

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
+use sea_orm::Database;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data_base = Database::connect("localhost:8080").await?;
+
+    
+
+    return Ok(());
 }
